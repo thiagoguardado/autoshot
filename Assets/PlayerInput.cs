60a0f4ae2a954f983ab2008c92055cf9b843de11
Rodrigo Nietiedt de Almeida <rodrigonietiedt@gmail.com>
2018-06-01 18:32:24 -0300

    build android

--- Assets/PlayerInput.cs
+++ Assets/PlayerInput.cs
@@ -5,15 +5,56 @@ using UnityEngine;
 
 public class PlayerInput : MonoBehaviour {
     public Character Character;
+    public bool touch_jump = false;
+    public bool touch_left = false;
+    public bool touch_right = false;
 
-	// Use this for initialization
-	void Start () {
+    public void pressRight()
+    {
+        touch_right = true;
+    }
+    public void releaseRight()
+    {
+        touch_right = false;
+    }
+    public void presLeft()
+    {
+        touch_left = true;
+    }
+    public void releaseLeft()
+    {
+        touch_left = false;
+    }
+    public void pressJump()
+    {
+        touch_jump = true;
+    }
+    public void releaseJump()
+    {
+        touch_jump = false;
+    }
+    // Use this for initialization
+    void Start () {
 		
 	}
 	
 	// Update is called once per frame
 	void Update () {
         Character.walkDirection.x = Input.GetAxis("Horizontal");
+        if(touch_left || touch_right)
+        {
+            if(touch_left)
+            {
+                Character.walkDirection.x = -1;
+            }
+            else if(touch_right)
+            {
+                Character.walkDirection.x = 1;
+            }        }
         Character.input_jumping = Input.GetButton("Jump");
+        if(touch_jump == true)
+        {
+            Character.input_jumping = true;
+        }
 	}
 }
