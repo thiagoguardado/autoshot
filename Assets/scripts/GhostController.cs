898a4fff38b5e15026202086d0474027c3a11530
Rodrigo Nietiedt de Almeida <rodrigonietiedt@gmail.com>
2018-06-12 19:14:53 -0300

    Ghost animations

---
+++ Assets/scripts/GhostController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Character))]
+public class GhostController : MonoBehaviour {
+    private Character _Character;
+    private Character _PlayerCharacter;
+    public void Awake()
+    {
+        PlayerInput pi = FindObjectOfType<PlayerInput>();
+        _PlayerCharacter = pi.GetComponent<Character>();
+        _Character = GetComponent<Character>();
+    }
+
+    public void Update()
+    {
+        _Character.InputWalkDirection.x = Mathf.Sign(_PlayerCharacter.transform.position.x - _Character.transform.position.x);
+        _Character.InputWalkDirection.y = Mathf.Sign(_PlayerCharacter.transform.position.y - _Character.transform.position.y);
+    }
+}
