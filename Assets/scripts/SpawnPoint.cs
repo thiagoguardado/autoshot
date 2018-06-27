2bd66a27f9549655a6bbfa41adf7207c27e89fea
Rodrigo Nietiedt de Almeida <rodrigonietiedt@gmail.com>
2018-06-27 10:16:54 -0300

    Generalizing Spawn Points to spawn weapons

---
+++ Assets/scripts/SpawnPoint.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(SpriteRenderer))]
+[SelectionBase]
+public class SpawnPoint : MonoBehaviour {
+    private SpriteRenderer _SpriteRenderer;
+
+    protected virtual void Awake()
+    {
+        _SpriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    public GameObject Spawn(GameObject prefab)
+    {
+        GameObject instance = Instantiate(prefab, GetSpawnPosition(), Quaternion.identity);
+        return instance;
+    }
+
+    private Vector3 GetSpawnPosition()
+    {
+        var min = _SpriteRenderer.bounds.min;
+        var max = _SpriteRenderer.bounds.max;
+        Vector2 pos = new Vector2();
+        pos.x = Random.Range(min.x, max.x);
+        pos.y = Random.Range(min.y, max.y);
+        return pos;
+    }
+}
---
+++ Assets/scripts/SpawnWaves/EnemySpawnPoint.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[SelectionBase]
+public class EnemySpawnPoint : SpawnPoint {
+    public int id = 0;    
+    private Text _textUi;
+
+    void OnValidate()
+    {
+        _textUi = GetComponentInChildren<Text>();
+        if(_textUi != null)
+        {
+            _textUi.text = id.ToString();
+        }
+        name = "SpawnPoint" + id.ToString();
+    }
+
+    protected override void Awake()
+    {
+        base.Awake();
+        GameManager.Instance.OnRequestEnemySpawn += OnSpawnEnemy;
+    }
+
+    void OnDestroy()
+    {
+        GameManager.Instance.OnRequestEnemySpawn -= OnSpawnEnemy;
+    }
+
+    void OnSpawnEnemy(GameObject prefab, int spawnPoint)
+    {
+        if(!enabled)
+        {
+            return;
+        }
+        if(spawnPoint == id)
+        {
+            var instance = Spawn(prefab);
+            GameManager.Instance.NotifyEnemySpawn(instance);
+        }
+    }   
+}
