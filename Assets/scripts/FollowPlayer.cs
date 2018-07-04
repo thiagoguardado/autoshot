5859c8b75062019fb0a08031829346346ddabf93
Thiago Guardado Beltran <beltran.thiago@gmail.com>
2018-07-04 14:00:57 -0300

    Camera Bounds

--- Assets/scripts/FollowPlayer.cs
+++ Assets/scripts/FollowPlayer.cs
@@ -1,10 +1,13 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class FollowPlayer : MonoBehaviour {
     public GameObject Player;
     public float Speed = 1;
+
+    public BoxCollider2D CameraBounds;
+
 	// Use this for initialization
 	void Start () {
 		
@@ -15,6 +18,9 @@ public class FollowPlayer : MonoBehaviour {
         
         Vector3 pos = transform.position;
         pos = Vector2.MoveTowards(transform.position, Player.transform.position, Speed * Time.deltaTime);
+
+        pos.x = Mathf.Clamp(pos.x, CameraBounds.bounds.min.x, CameraBounds.bounds.max.x);
+        pos.y = Mathf.Clamp(pos.y, CameraBounds.bounds.min.y, CameraBounds.bounds.max.y);
         pos.z = transform.position.z;
         transform.position = pos;
 	}
